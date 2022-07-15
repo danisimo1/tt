5460a7d72719d60060b530f906caba4badc5939a
Cheremisin Daniil <thedenchill2000@mail.ru>
2022-07-15 16:43:31 +0300

    new commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <form v-if="visible" @submit.prevent>
+    <input type="text" placeholder="Имя" v-model="name"><br><br>
+    <input type="text" placeholder="Фамилия" v-model="secName"><br><br>
+    <input type="text" placeholder="Отчество" v-model="thName"><br><br>
+    <input type="text" placeholder="Должность" v-model="job"><br><br>
+    <input type="checkbox" value="Трудовая книжка сдана">Трудовая книжка сдана<br><br>
+    <input type="text" placeholder="Оклад"><br><br>
+    <input type="date"/><br><br>
+    <input type="radio" v-model="type" name="question">
+    <label>Полная ставка</label>
+    <br>
+    <input type="radio" v-model="type" name="question">
+    <label>Половина ставки</label><br><br>
+    <button @click="newEmp">Создать</button><br><br>
+  </form>
+  <button @click="visible = !visible">Добавить сотрудника</button>
+  <br>
+  <br>
+  <ol>
+    <li v-for="employe in employees" v-bind:key="employe.id">
+      {{employe.secondName + ' ' + employe.firstName + ' ' + employe.thirdName + ' - ' + employe.position}} 
+      <button @click="deleteEmp">Удалить</button>
+    </li>
+  </ol>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      employees: [
+        {firstName: 'Альберт', secondName: 'Алмазов', thirdName: 'Альбертович', position: 'frontend-разработчик'},
+        {firstName: 'Иван', secondName: 'Иванов', thirdName: 'Иванович', position: 'backend-разработчик'},
+        {firstName: 'Дильшот', secondName: 'Ахматов', thirdName: 'Александрович', position: 'devOps-инженер'}
+      ],
+      visible: false,
+      type: '',
+      name: '',
+      secName: '',
+      thName: '',
+      job: ''
+    }
+  },
+  methods: {
+    newEmp(){
+      const newEmpl = {firstName: this.name, secondName: this.secName, thirdName: this.thName, position: this.job}
+      this.employees.push(newEmpl)
+      this.name = ''
+      this.secName = ''
+      this.thName = ''
+      this.job = ''
+    },
+    deleteEmp(employe){
+      this.employees = this.employees.filter(p => p.id !== employe.id)
+    }
   }
 }
 </script>
@@ -19,8 +65,13 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+form{
+  border: 1px solid black;
+  width: 500px;
+  padding: 15px;
 }
 </style>
